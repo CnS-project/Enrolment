c26ba432b1fe3526fbf8ee3b43f452bbca00ddab
bae-sh <hyeon1293@gmail.com>
2022-11-23 00:46:50 +0900

    chore : list 값 수정

--- client/src/Lecture.js
+++ client/src/Lecture.js
@@ -3,32 +3,34 @@ import { registerCourse } from './api/api';
 import './styles/lecture.css';
 export default function Leacture() {
   const THEAD_LIST = [
-    '신청',
-    '과목',
-    '과목 번호',
-    '분반',
-    '학과',
-    '학년',
-    '교수',
-    '학점',
-    '정원',
+    { key: 'register', title: '신청' },
+    { key: 'name', title: '과목' },
+    { key: 'courseNumber', title: '과목 번호' },
+    { key: 'classNumber', title: '분반' },
+    { key: 'department', title: '학과' },
+    { key: 'targetGrade', title: '학년' },
+    { key: 'professor', title: '교수' },
+    { key: 'credit', title: '학점' },
+    { key: 'capacity', title: '정원' },
   ];
   const [word, setWord] = useState('');
   const [courses, setCourses] = useState([]);
   const [courseNumber, setCourseNumber] = useState();
   const [classNumber, setClassNumber] = useState();
-  const [sortTarget, setsortTarget] = useState({ thNumber: 0, count: 0 });
+  const [sortTarget, setsortTarget] = useState({ key: 'name', count: 0 });
   const arrowMark = ['', '↑', '↓'];
 
-  // useEffect(() => {
-  //   setLeactureDatas((prev) => {
-  //     return prev.sort((a, b) => {
-  //       if (sortTarget.count === 1) {
-  //         return a.thNumber - b.thNumber;
-  //       } else if (sortTarget.count === 2) return b.thNumber - a.thNumber;
-  //     });
-  //   });
-  // }, [sortTarget]);
+  useEffect(() => {
+    setCourses((prev) => {
+      return prev.sort((a, b) => {
+        if (sortTarget.count === 1) {
+          return a[sortTarget.key] - b[sortTarget.key];
+        } else if (sortTarget.count === 2)
+          return b[sortTarget.key] - a[sortTarget.key];
+        return false;
+      });
+    });
+  }, [sortTarget]);
   return (
     <div>
       <h1>수강 신청</h1>
@@ -66,25 +68,28 @@ export default function Leacture() {
           </button>
         </label>
       </div>
-
+      <div>항목 클릭 시 정렬 가능</div>
       <table id="lecture-table">
         <thead>
-          {THEAD_LIST.map((title, index) => (
-            <td
-              id={title}
-              onClick={() => {
-                setsortTarget((prev) => {
-                  if (prev.thNumber === index)
-                    return { ...prev, count: (prev.count + 1) % 3 };
-                  return { thNumber: index, count: 1 };
-                });
-              }}
-            >
-              {sortTarget.thNumber === index
-                ? title + arrowMark[sortTarget.count]
-                : title}
-            </td>
-          ))}
+          <tr>
+            {THEAD_LIST.map(({ title }, index) => (
+              <th
+                key={title}
+                onClick={() => {
+                  if (index !== 0)
+                    setsortTarget((prev) => {
+                      if (prev.thNumber === index)
+                        return { ...prev, count: (prev.count + 1) % 3 };
+                      return { thNumber: index, count: 1 };
+                    });
+                }}
+              >
+                {sortTarget.thNumber === index
+                  ? title + arrowMark[sortTarget.count]
+                  : title}
+              </th>
+            ))}
+          </tr>
         </thead>
         <tbody>
           <tr>
