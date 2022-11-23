93672b78342cc9ff2f573e6471eb63261038a259
Im_younique <48540492+DongYounYim@users.noreply.github.com>
2022-11-23 22:11:07 +0900

    최종 작업 전 merge

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import Login from './login';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Leacture from './Lecture';
 import LookUp from './LookUp';
+import Admin from './Admin';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -15,6 +15,7 @@ root.render(
           <Route path="/" element={<Login />}></Route>
           <Route path="/lecture" element={<Leacture />}></Route>
           <Route path="/lookUp" element={<LookUp />}></Route>
+          <Route path="/admin" element={<Admin />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
